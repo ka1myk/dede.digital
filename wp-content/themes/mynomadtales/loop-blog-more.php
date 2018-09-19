73da3aba5dd123afa1e03c4b6d0858098942fd50
MauntyStudio <maunty-service@yandex.ru>
2018-09-19 11:05:41 +0300

    final_website

---
+++ wp-content/themes/mynomadtales/loop-blog-more.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Запись в цикле (loop.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */ 
+ 	 $color = get_field( 'bg_color'); 
+	 $anons_img = get_field( 'anons_img'); 
+	 $subtitle = get_field( 'subtitle'); 
+	 $anons = get_field( 'anons'); 
+	
+	//echo '<pre>'; print_r( $anons_img); echo '</pre>';
+	
+	$h = $anons_img['height'] / ($anons_img['width'] / 360);
+	
+	$scale = min(360 / $anons_img['width'], 500 / $anons_img['height'] );
+
+	if ($scale >= 1) {
+		$height = $anons_img['height'];
+	}else{
+		$height = $anons_img['height'] * $scale;
+	}
+		
+
+?>
+
+	<div class="post-wrap grid-item " >
+	<a class="post-link " style="background-color:<?=$color?>" href="<?=the_permalink();?>">
+    
+     	
+            <div class="anons-img-wrap" style=" height:<?=$height?>px"><div class="anons-img anim" style="background-image:url(<?=$anons_img['url'];?>)"></div></div>
+            <div class="anons-text">
+    	 	<h2><?php the_title(); ?></h2>
+    	 	<h3><?=$subtitle?></h3>
+    	 	<p><?=$anons?></p>
+            </div>
+             
+     </a>
+	</div>
+  
+	
