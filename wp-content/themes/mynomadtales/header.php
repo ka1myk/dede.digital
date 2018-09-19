9a34a61c7808c5905f0f3c3ac4579ab15a2eb0ab
MauntyStudio <maunty-service@yandex.ru>
2018-09-19 09:50:22 +0300

    v7_website

---
+++ wp-content/themes/mynomadtales/header.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Шаблон шапки (header.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); // вывод атрибутов языка ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); // кодировка ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="<?=get_template_directory_uri()?>/js/jquery.fancybox.min.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/mynomadtales/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/mynomadtales/overheight/overheight.min.css">
+	<?php /* Все скрипты и стили теперь подключаются в functions.php */ ?>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="<?=get_template_directory_uri()?>/overheight/overheight.min.js"> </script>  
+	
+    <? /* Скрипты только для главной страницы */  ?>
+	<?	if(is_front_page()): ?>
+		<link rel="stylesheet" type="text/css" href="<?=get_template_directory_uri()?>/slick/slick.min.css">
+		<link rel="stylesheet" type="text/css" href="<?=get_template_directory_uri()?>/map/map.min.css">
+		<link rel="stylesheet" type="text/css" href="<?=get_template_directory_uri()?>/countdown/jquery.countdown.min.css">
+		<link rel="stylesheet" type="text/css" href="<?=get_template_directory_uri()?>/fullPage.js-master/dist/fullpage.min.css">
+		
+		<script src="<?=get_template_directory_uri()?>/countdown/jquery.countdown.min.js"> </script>
+        <script src="<?=get_template_directory_uri()?>/slick/slick.min.js"></script>
+        <script src="<?=get_template_directory_uri()?>/map/map.min.js"></script>
+        <script src="<?=get_template_directory_uri()?>/fullPage.js-master/dist/fullpage.min.js"></script>
+	<? endif;?>
+
+	<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<?php wp_head(); // необходимо для работы плагинов и функционала ?>
+</head>
+<body>
+	<? if(is_front_page()){  get_template_part('slider');  } ?>
+    <header>
+    	<div class="header">
+    	<div class="fix <?=(is_front_page()) ? 'home': ''?>">
+        <div class="wrapper">
+        	<div class="logo anim"><a href="/" title=""></a></div>
+        	<div class="mobile-menu-button hide-760"><a href="/" onclick="content_overheight(this); return false;" css="menu" content="header-menu social-links"><span></span></a></div>
+            <div class="header-menu-wrap show-760" id="header-menu">
+            <?php
+				wp_nav_menu([
+					'container' => 'div',
+					'container_class' => 'header-menu', //класс div блока
+					'menu_class' => '', //класс меню
+					'theme_location' => 'top'
+			  	]);
+ 			?>
+        	</div>
+           </div> 
+        </div>
+		</div>
+	</header>
+   <div class="fullpage" id="fullpage"> 
